44cef3d37596d1ab68a04c72ae1f56fcfe57c970
Aytaç Cici <aytaccici@gmail.com>
2022-07-31 18:59:25 +0300

    Pagination value is set 4

--- config/app.php
+++ config/app.php
@@ -127,7 +127,7 @@ return [
      * Default pagination value
      */
 
-    'pagination_value' => 2,
+    'pagination_value' => 4,
 
     /*
     |--------------------------------------------------------------------------
