2d0781148b311ebe1dcef5083c5d6a6e460c53ab
Aytaç Cici <aytaccici@gmail.com>
2022-07-31 22:13:46 +0300

    A few example unit test

--- config/database.php
+++ config/database.php
@@ -34,15 +34,6 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -89,6 +80,14 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
     ],
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,29 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function initDatabase()
+    {
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+
+    protected function resetDatabase()
+    {
+        Artisan::call('migrate:reset');
+    }
+
+    protected function tearDown(): void
+    {
+        $config = app('config');
+        $request  = app('request');
+        parent::tearDown();
+        app()->instance('config', $config);
+        app()->instance('request', $request);
+    }
 }
