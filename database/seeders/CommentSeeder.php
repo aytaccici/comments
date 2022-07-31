615609ecebbd073ac2f2b05d9112f6aeb092fc88
Aytaç Cici <aytaccici@gmail.com>
2022-07-31 22:23:27 +0300

    Seeder is ready for 3 level comment

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -8,6 +8,27 @@ class CommentSeeder extends Seeder
 {
     public function run()
     {
-        \App\Models\Comment::factory(5)->create();
+       $level1 =  \App\Models\Comment::factory(1)->create(
+            [
+                'post_id' =>  1,
+                'level' => 1
+            ]
+        );
+
+        $level2 =  \App\Models\Comment::factory(1)->create(
+            [
+                'post_id' =>  1,
+                'level' => '2',
+                'parent_id' => data_get($level1->first(),'id')
+            ]
+        );
+
+        $level3 =  \App\Models\Comment::factory(1)->create(
+            [
+                'post_id' =>  1,
+                'level' => '3',
+                'parent_id' => data_get($level2->first(),'id')
+            ]
+        );
     }
 }
